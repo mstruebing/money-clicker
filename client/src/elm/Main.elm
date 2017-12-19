da5e861eb8561afccdd1c9bb532ad2694e67620b
Max Strübing <mxstrbng@gmail.com>
2017-12-19 12:58:54 +0100

    initial commit

---
+++ client/src/elm/Main.elm
@@ -0,0 +1,48 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text)
+
+
+-- APP
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+model : Model
+model =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "hallo" ]
