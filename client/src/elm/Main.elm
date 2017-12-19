16d67e8de5120b05c3a56c067dc6a36c99e5fa84
Max Strübing <mxstrbng@gmail.com>
2017-12-19 14:31:10 +0100

    feat(saving): save current game to local storage

--- client/src/elm/Main.elm
+++ client/src/elm/Main.elm
@@ -1,6 +1,17 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, img, text)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+import Json.Decode as Json exposing (Decoder, Value)
+import Json.Decode.Pipeline as Json
+
+
+-- Ports
+
+
+port saveGameToLocalStorage : Model -> Cmd msg
+
 
 
 -- APP
@@ -8,7 +19,12 @@ import Html exposing (Html, div, text)
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -16,12 +32,37 @@ main =
 
 
 type alias Model =
-    Int
+    { money : Int
+    , clicks : Int
+    , multiplier : Float
+    }
 
 
-model : Model
-model =
-    0
+initialModel : Model
+initialModel =
+    { money = 0, clicks = 0, multiplier = 1 }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- DECODER
+
+
+saveGameDecoder : Decoder Model
+saveGameDecoder =
+    Json.decode Model
+        |> Json.optional "money" Json.int 0
+        |> Json.optional "clicks" Json.int 0
+        |> Json.optional "multiplier" Json.float 1
 
 
 
@@ -30,13 +71,20 @@ model =
 
 type Msg
     = Increment
+    | ReadGame (Maybe Model)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            ( { model | clicks = model.clicks + 1, money = model.money + round (1 * model.multiplier) }, saveGameToLocalStorage model )
+
+        ReadGame Nothing ->
+            ( model, Cmd.none )
+
+        ReadGame maybeModel ->
+            ( initialModel, Cmd.none )
 
 
 
@@ -45,4 +93,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "hallo" ]
+    div [ class "site__wrapper" ]
+        [ div [ class "score" ] [ text <| "current money: " ++ toString model.money ++ " $" ]
+        , img [ class "clicker", src "static/img/clicker.png", onClick Increment ] []
+        ]
--- client/src/static/index.js
+++ client/src/static/index.js
@@ -1,5 +1,10 @@
 // pull in desired CSS/SASS files
 require('./styles/main.scss');
 
-var Elm = require('../elm/Main');
-Elm.Main.embed(document.getElementById('main'));
+const MoneyClicker = require('../elm/Main');
+const app = MoneyClicker.Main.embed(document.getElementById('main'));
+
+app.ports.saveGameToLocalStorage.subscribe(model => {
+    localStorage.setItem("savegame", JSON.stringify(model));
+});
+
